d7a266c3e2b99f9dceb691c764662452a21c853e
Developer Shawon <sksagor7172@gmail.com>
2022-01-18 20:45:18 +0600

    update all

--- app.js
+++ app.js
@@ -10,12 +10,42 @@ const resett = document.querySelector('#resett');
 
 
 
+
 checkButton.addEventListener('click', function(){
 
 pp.innerHTML = agecall(name.value, years.value);
 
+if(name.value == '' || years.value == ''){
+
+    pp.innerHTML = `<p class="alert alert-danger">All filds are requerd</p>`
+    
+    }
 
 });
+
+
+
+
+
+years.addEventListener('keyup', ()=>{
+
+    let patten = /^[0-9]*$/;
+    console.log(patten.test(years.value));
+
+    if(patten.test(years.value) == false){
+
+        pp.innerHTML = `<p class="alert alert-danger text-center"> Input Only Number </p>`
+     }else{
+         pp.innerHTML = '';
+     }
+     
+     if (years.value == ''){
+
+        pp.innerHTML = '';
+     }
+   
+
+})
 
 resett.addEventListener('click', ()=>{
 
@@ -23,3 +53,4 @@ name.value = '';
 years.value = '';
 pp.innerHTML = '';
 });
+
